8dbd9ef0b55fa169f0821274d959e19ee484d63c
陈淇格 <cqg186495@outlook.com>
2018-07-02 16:12:43 +0800

    no message

---
+++ src/components/Header.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class = "header">
+        <div class="header-title">
+            zOS frontend
+        </div>
+        <div class="input-container">
+            <el-input v-model="input" placeholder="Please enter catalog" prefix-icon="el-icon-search" clearable></el-input>
+        </div>
+        <div class="nav-container">
+            <el-menu :default-active="activeIndex" mode="horizontal" @select="handleSelect" background-color="#D2E0E9">
+                <el-menu-item index="1">Main catalog</el-menu-item>
+                <el-menu-item index="2">User catalog</el-menu-item>
+                <el-menu-item index="3">Alias</el-menu-item>
+                <el-menu-item index="4">Chart</el-menu-item>
+            </el-menu>
+        </div>
+    </div>
+</template>
+
+<script>
+	import Vue from 'vue'
+	import ElementUI from 'element-ui'
+
+    export default {
+        name: 'Header',
+        data () {
+            return {
+                msg: 'Welcome to Your Vue.js App',
+                input:'',
+                activeIndex: '1'
+            }
+        },
+	    methods: {
+		    handleSelect(key, keyPath) {
+			    console.log(key, keyPath)
+		    }
+	    }
+    }
+</script>
+
+<style scoped>
+    .header{
+        width: 100%;
+        height: 13rem;
+        position: fixed;
+        left: 0;
+        top:0;
+        background-color: rgb(210,224,233);
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        font-family: "Century Gothic";
+    }
+    .header-title{
+        font-size: 2em;
+        font-family: "Century Gothic";
+    }
+    .input-container{
+        margin-top:20px;
+        width: 300px;
+        font-family: "Century Gothic";
+    }
+
+    .nav-container{
+        font-family: "Century Gothic";
+        margin-top:20px;
+
+    }
+</style>
---
+++ src/pages/home.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class = "container">
+        <router-view/>
+        <Header></Header>
+    </div>
+</template>
+
+<script>
+	import Header from '../components/Header'
+    import MainCatalog from '../components/MainCatalog'
+
+	export default {
+		name: 'home',
+		data () {
+			return {
+
+			}
+		},
+		components:{
+			Header,
+            MainCatalog
+		}
+	}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .container{
+        display: flex;
+        flex-direction: row;
+    }
+
+
+</style>
