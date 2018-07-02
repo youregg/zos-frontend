1c017b6ad4e4ac299d02f4b0382141d9fced091f
陈淇格 <cqg186495@outlook.com>
2018-07-02 16:12:18 +0800

    init header

---
+++ src/components/MainCatalog.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+
+<script>
+    export default{
+    	name:"MainCatalog",
+        data(){
+    		return{
+
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+
+</style>
