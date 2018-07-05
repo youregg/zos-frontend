b309ee91c8c66d305a75f0dd40c9276f96bdd60b
陈淇格 <cqg186495@outlook.com>
2018-07-05 15:37:53 +0800

    no message

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -146,260 +146,260 @@
 
 <script>
 	export default {
-        name:"Search",
-        methods: {
-	        indexMethod(index) {
-		        return index + 1
-	        },
-            switchOption(e){
-	        	if(e==1){
-                    $("#alias-container").css('display','none')
-                    $("#usercatalog-container").show()
-                }
-                else if(e==2){
-                    $("#usercatalog-container").css('display','none')
-                    $("#alias-container").show()
-                }
+		name:"Search",
+		methods: {
+			indexMethod(index) {
+				return index + 1
+			},
+			switchOption(e){
+				if(e==1){
+					$("#alias-container").css('display','none')
+					$("#usercatalog-container").show()
+				}
+				else if(e==2){
+					$("#usercatalog-container").css('display','none')
+					$("#alias-container").show()
+				}
 
-            },
-	        search(){
-            	if(this.select==1){
-            		//user catalog
-                    let _this = this
-                    _this.tableData10 = []
-                    _this.tableData11 = []
-                    _this.tableData12 = []
-                    _this.$http({
-                        url:'http://127.0.0.1:3000/catalog/'+ this.input,
-                        method:'get'
-                    }).then(function (res) {
-                        let jobId = res.data.jobid
-                        let jobName = res.data.jobname
-                        _this.$http({
-                            url:'http://127.0.0.1:3000/getJobById/'+jobName+"/"+jobId,
-                            method:'get'
-                        }).then(function(res){
-                            _this.dealCatalog(res.data)
-                            _this.dealVolumes(res.data)
-                            _this.dealAssociation(res.data)
+			},
+			search(){
+				if(this.select==1){
+					//user catalog
+					let _this = this
+					_this.tableData10 = []
+					_this.tableData11 = []
+					_this.tableData12 = []
+					_this.$http({
+						url:'http://127.0.0.1:3000/catalog/'+ this.input,
+						method:'get'
+					}).then(function (res) {
+						let jobId = res.data.jobid
+						let jobName = res.data.jobname
+						_this.$http({
+							url:'http://127.0.0.1:3000/getJobById/'+jobName+"/"+jobId,
+							method:'get'
+						}).then(function(res){
+							_this.dealCatalog(res.data)
+							_this.dealVolumes(res.data)
+							_this.dealAssociation(res.data)
 
-                        })
-                    }).catch(function(error){
-                        console.log(error)
-                    })
-                }else if(this.select==2){
-            		//ALIAS
-                    let _this = this
-                    _this.tableData13 = []
-                    _this.tableData14 = []
-                    _this.$http({
-                        url:'http://127.0.0.1:3000/alias/'+ this.input,
-                        method:'get'
-                    }).then(function (res) {
-                        let jobId = res.data.jobid
-                        let jobName = res.data.jobname
-                        _this.$http({
-                            url:'http://127.0.0.1:3000/getJobById/'+jobName+"/"+jobId,
-                            method:'get'
-                        }).then(function(res){
-                            _this.dealAlias(res.data)
-                            _this.dealAliasAssociation(res.data)
-                        })
-                    }).catch(function(error){
-                        console.log(error)
-                    })
+						})
+					}).catch(function(error){
+						console.log(error)
+					})
+				}else if(this.select==2){
+					//ALIAS
+					let _this = this
+					_this.tableData13 = []
+					_this.tableData14 = []
+					_this.$http({
+						url:'http://127.0.0.1:3000/alias/'+ this.input,
+						method:'get'
+					}).then(function (res) {
+						let jobId = res.data.jobid
+						let jobName = res.data.jobname
+						_this.$http({
+							url:'http://127.0.0.1:3000/getJobById/'+jobName+"/"+jobId,
+							method:'get'
+						}).then(function(res){
+							_this.dealAlias(res.data)
+							_this.dealAliasAssociation(res.data)
+						})
+					}).catch(function(error){
+						console.log(error)
+					})
 
-                }
+				}
 
-	        },
+			},
 
-	        dealCatalog(res){
-                let _this = this
-                let lines = res.split('\n')
-                let temp = {
-				        "Name":_this.input,
-				        "Incat":"",
-				        "Release":"",
-                }
-                for(let i = 0; i<lines.length;i++){
-                    if(lines[i].indexOf("IN-CAT")!= -1){
-                        temp.Incat = lines[i].match(/IN-CAT --- (\S*)/)[1]
-                    }
-                    if(lines[i].indexOf("RELEASE")!=-1){
-                    	temp.Release = lines[i].match(/RELEASE----------------(\S*)/)[1]
-                    }
-                }
-                _this.tableData10.push(temp)
-                temp={
-	                "Name":"",
-	                "Incat":"",
-	                "Release":"",
-                }
-            },
+			dealCatalog(res){
+				let _this = this
+				let lines = res.split('\n')
+				let temp = {
+					"Name":_this.input,
+					"Incat":"",
+					"Release":"",
+				}
+				for(let i = 0; i<lines.length;i++){
+					if(lines[i].indexOf("IN-CAT")!= -1){
+						temp.Incat = lines[i].match(/IN-CAT --- (\S*)/)[1]
+					}
+					if(lines[i].indexOf("RELEASE")!=-1){
+						temp.Release = lines[i].match(/RELEASE----------------(\S*)/)[1]
+					}
+				}
+				_this.tableData10.push(temp)
+				temp={
+					"Name":"",
+					"Incat":"",
+					"Release":"",
+				}
+			},
 
-            dealVolumes(res){
-	        	let _this = this
-	            let lines = res.split('\n')
+			dealVolumes(res){
+				let _this = this
+				let lines = res.split('\n')
 
-                for(let i = 0;i<lines.length;i++){
-	        		if(lines[i].indexOf("VOLUMES")!=-1){
-	        			let tempLine = lines[i+1].replace(/(^\s*)|(\s*$)/g,"")//去首尾空格
-                        let arr = tempLine.split("     ")
-                        for(let j = 0 ;j < arr.length;j++){
-	                        let temp={
-		                        "VolumeType":"",
-		                        "VolumeName":""
-	                        }
-	                        temp.VolumeType = arr[j].substring(0,arr[j].indexOf('-'))
-                            temp.VolumeName = arr[j].substring(arr[j].lastIndexOf('-')+1,arr[j].length)
-                            _this.tableData11.push(temp)
-                            temp={
-	                            "VolumeType":"",
-	                            "VolumeName":""
-                            }
-                        }
+				for(let i = 0;i<lines.length;i++){
+					if(lines[i].indexOf("VOLUMES")!=-1){
+						let tempLine = lines[i+1].replace(/(^\s*)|(\s*$)/g,"")//去首尾空格
+						let arr = tempLine.split("     ")
+						for(let j = 0 ;j < arr.length;j++){
+							let temp={
+								"VolumeType":"",
+								"VolumeName":""
+							}
+							temp.VolumeType = arr[j].substring(0,arr[j].indexOf('-'))
+							temp.VolumeName = arr[j].substring(arr[j].lastIndexOf('-')+1,arr[j].length)
+							_this.tableData11.push(temp)
+							temp={
+								"VolumeType":"",
+								"VolumeName":""
+							}
+						}
 
-                    }
-                }
-            },
+					}
+				}
+			},
 
-	        dealAssociation(res){
-		        let _this = this
-                let lines = res.split('\n')
+			dealAssociation(res){
+				let _this = this
+				let lines = res.split('\n')
 
-                for(let i = 0;i < lines.length;i++){
-                    if(lines[i].indexOf("ALIAS----")!=-1){
-                        let tempLine = lines[i].replace(/(^\s*)|(\s*$)/g,"")//去首尾空格
-	                    let temp={
-		                    "AssociationType":"",
-		                    "AssociationName":""
-	                    }
-	                    temp.AssociationType = tempLine.substring(0,tempLine.indexOf('-'))
-                        temp.AssociationName = tempLine.substring(tempLine.lastIndexOf('-')+1,tempLine.length)
-                        _this.tableData12.push(temp)
-                        temp={
-	                        "AssociationType":"",
-	                        "AssociationName":""
-                        }
-                    }
-                }
-	        },
+				for(let i = 0;i < lines.length;i++){
+					if(lines[i].indexOf("ALIAS----")!=-1){
+						let tempLine = lines[i].replace(/(^\s*)|(\s*$)/g,"")//去首尾空格
+						let temp={
+							"AssociationType":"",
+							"AssociationName":""
+						}
+						temp.AssociationType = tempLine.substring(0,tempLine.indexOf('-'))
+						temp.AssociationName = tempLine.substring(tempLine.lastIndexOf('-')+1,tempLine.length)
+						_this.tableData12.push(temp)
+						temp={
+							"AssociationType":"",
+							"AssociationName":""
+						}
+					}
+				}
+			},
 
-	        dealAlias(res){
-                let _this = this
-                let lines = res.split('\n')
-                let temp = {
-                    "Name":_this.input,
-                    "Incat":"",
-                    "Release":"",
-                    "Creation":""
-                }
-                for(let i = 0; i < lines.length;i++){
-                    if(lines[i].indexOf("IN-CAT")!= -1){
-                        temp.Incat = lines[i].match(/IN-CAT --- (\S*)/)[1]
-                    }
-                    if(lines[i].indexOf("RELEASE")!=-1){
-                        temp.Release = lines[i].match(/RELEASE----------------(\S*)/)[1]
-                    }
-                    if(lines[i].indexOf("CREATION")!=-1){
-                    	temp.Creation = lines[i].match(/CREATION--------(\S*)/)[1]
-                    }
-                }
-                _this.tableData13.push(temp)
-            },
+			dealAlias(res){
+				let _this = this
+				let lines = res.split('\n')
+				let temp = {
+					"Name":_this.input,
+					"Incat":"",
+					"Release":"",
+					"Creation":""
+				}
+				for(let i = 0; i < lines.length;i++){
+					if(lines[i].indexOf("IN-CAT")!= -1){
+						temp.Incat = lines[i].match(/IN-CAT --- (\S*)/)[1]
+					}
+					if(lines[i].indexOf("RELEASE")!=-1){
+						temp.Release = lines[i].match(/RELEASE----------------(\S*)/)[1]
+					}
+					if(lines[i].indexOf("CREATION")!=-1){
+						temp.Creation = lines[i].match(/CREATION--------(\S*)/)[1]
+					}
+				}
+				_this.tableData13.push(temp)
+			},
 
-	        dealAliasAssociation(res){
-		        let _this = this
-		        let lines = res.split('\n')
-		        let temp = {
-			        "AssociationType":"",
-                    "AssociationName":"",
-		        }
-		        for(let i = 0; i < lines.length;i++){
-			        if(lines[i].indexOf("ASSOCIATIONS")!= -1){
-			        	let tempLine = lines[i+1].replace(/(^\s*)|(\s*$)/g,"")
-                        temp.AssociationType = tempLine.substring(0,tempLine.indexOf('-'))
-                        temp.AssociationName = tempLine.substring(tempLine.lastIndexOf('-')+1,tempLine.length)
-			        }
-		        }
-		        _this.tableData14.push(temp)
-	        }
-        },
+			dealAliasAssociation(res){
+				let _this = this
+				let lines = res.split('\n')
+				let temp = {
+					"AssociationType":"",
+					"AssociationName":"",
+				}
+				for(let i = 0; i < lines.length;i++){
+					if(lines[i].indexOf("ASSOCIATIONS")!= -1){
+						let tempLine = lines[i+1].replace(/(^\s*)|(\s*$)/g,"")
+						temp.AssociationType = tempLine.substring(0,tempLine.indexOf('-'))
+						temp.AssociationName = tempLine.substring(tempLine.lastIndexOf('-')+1,tempLine.length)
+					}
+				}
+				_this.tableData14.push(temp)
+			}
+		},
 
-        data() {
-    	    return {
-              tableData10:[],
-              tableData11:[],
-              tableData12:[],
-              tableData13:[],
-              tableData14:[],
-              catalogMenu:'1',
-              volumesMenu:'1',
-              associationMenu:'1',
-              aliasAssociationMenu:'1',
-              aliasMenu:'1',
-              input:"",
-              select:"",
-              option:0
-            }
-        }
+		data() {
+			return {
+				tableData10:[],
+				tableData11:[],
+				tableData12:[],
+				tableData13:[],
+				tableData14:[],
+				catalogMenu:'1',
+				volumesMenu:'1',
+				associationMenu:'1',
+				aliasAssociationMenu:'1',
+				aliasMenu:'1',
+				input:"",
+				select:"",
+				option:0
+			}
+		}
 
-    }
+	}
 
 
 </script>
 
 <style scoped>
-  .container{
-      display: flex;
-      flex-direction: column;
-      font-size: 13px;
-      width: 90%;
-      margin-top: 100px;
-      align-items: center;
-      justify-content: center;
-  }
+    .container{
+        display: flex;
+        flex-direction: column;
+        font-size: 13px;
+        width: 90%;
+        margin-top: 100px;
+        align-items: center;
+        justify-content: center;
+    }
 
-  .table{
-      display: flex;
-      flex-direction: column;
-      font-size: 13px;
-      width:90%;
-      margin-top: 50px;
-  }
+    .table{
+        display: flex;
+        flex-direction: column;
+        font-size: 13px;
+        width:90%;
+        margin-top: 50px;
+    }
 
-  .el-input{
-      width: 600px;
-  }
-  .el-select{
-      width: 180px;
-  }
+    .el-input{
+        width: 600px;
+    }
+    .el-select{
+        width: 180px;
+    }
 
-  #input-container{
-      display: flex;
-      margin-top:20px;
-      font-family: "Century Gothic";
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-  }
+    #input-container{
+        display: flex;
+        margin-top:20px;
+        font-family: "Century Gothic";
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
 
-  #usercatalog-container{
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      width: 100%;
-  }
+    #usercatalog-container{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+    }
 
-  #alias-container{
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: center;
-      width: 100%;
+    #alias-container{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
 
 
-  }
+    }
 
 </style>
